c771afb6ca112a0a93bfd7965b1fc59c2d1db9ea
Brittanyb94 <30817565+Brittanyb94@users.noreply.github.com>
2025-04-15 18:26:09 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -2,19 +2,16 @@ const svg = d3.select("#barChart svg");
 const chartWidth = 600;
 const chartHeight = 300;
 
-// Set SVG dimensions
 svg.attr("width", chartWidth).attr("height", chartHeight);
 
-// Simulated dataset
 const dataMap = {
   Texas: { "Title I": 48, "IDEA": 35, "Pell Grants": 40 },
   Mississippi: { "Title I": 60, "IDEA": 45, "Pell Grants": 50 },
   California: { "Title I": 30, "IDEA": 25, "Pell Grants": 35 }
 };
 
-// Update the chart
 function updateChart(state, program) {
-  svg.selectAll("*").remove(); // Clear chart
+  svg.selectAll("*").remove();
 
   const value = dataMap[state][program];
 
@@ -33,12 +30,10 @@ function updateChart(state, program) {
     .attr("fill", "#000");
 }
 
-// Initial load
 let selectedState = document.getElementById("state").value;
 let selectedProgram = document.getElementById("program").value;
 updateChart(selectedState, selectedProgram);
 
-// Event listeners
 document.getElementById("state").addEventListener("change", function () {
   selectedState = this.value;
   updateChart(selectedState, selectedProgram);
